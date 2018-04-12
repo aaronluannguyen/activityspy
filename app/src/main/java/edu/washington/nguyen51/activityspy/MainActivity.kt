aa6ecdae192b94ff3801143e6762d0f43943aaf2
Student User <studentuser@Students-MacBook-Pro.local>
2018-04-11 18:52:15 -0700

    Saving changes made to lifecycle logging

--- app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
@@ -1,12 +1,22 @@
 package edu.washington.nguyen51.activityspy
 
+import android.nfc.Tag
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+const val TAG = "Main Activity"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "on $TAG event fired" + intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain!")
     }
 }
