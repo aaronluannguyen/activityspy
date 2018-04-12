b1c2da5dbbf5a5a15c6aa1e798edb3548b31b1d9
Student User <studentuser@Students-MacBook-Pro.local>
2018-04-11 18:52:35 -0700

    Fixed typo for onCreate event

--- app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG, "on $TAG event fired" + intent)
+        Log.i(TAG, "onCreate event fired" + intent)
     }
 
     override fun onDestroy() {
