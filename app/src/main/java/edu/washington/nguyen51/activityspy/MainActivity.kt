f7029545f9affcbf03026ba9b642608c0ae60a0e
Student User <studentuser@Students-MacBook-Pro.local>
2018-04-11 19:25:07 -0700

    Added diagnostic txt file

--- app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/nguyen51/activityspy/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i(TAG, "onCreate event fired" + intent)
+        Log.i(TAG, "onCreate event fired: intent{" + intent + "}")
     }
 
     override fun onDestroy() {
